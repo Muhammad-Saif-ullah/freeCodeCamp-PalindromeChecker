91555d71cdd78b51c81b43a7df67ffb572c76e05
Muhammad Saifullah <110117071+Muhammad-Saif-ullah@users.noreply.github.com>
2024-03-20 12:16:46 +0500

    Added comments in script.js

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const inputEl = document.getElementById('text-input');
 const checkBtnEl = document.getElementById('check-btn');
 const resultEl = document.getElementById('result');
 
+// A function that removes the non-alphabets from a string
 const removeNonAlphabets = str => {
   let newStr = str.replace(/[^a-zA-Z0-9]/g,'');
   return newStr.toLowerCase();
 }
 
+// A function that checks whether a provided string is a palindrome or not
 const checkPalindrome = str => {
   if (str.length === 1) {
     return true;
@@ -19,6 +21,7 @@ const checkPalindrome = str => {
   }
 }
 
+// A function that is called when the check button is clicked
 const checkBtnClicked = () => {
   const inputValue = inputEl.value;
 
